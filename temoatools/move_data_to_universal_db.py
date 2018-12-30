ab513e7752d9667cca1447335c2ba3b05e2d8949
EnergyModels <jab6ft@virginia.edu>
2018-12-30 18:21:54 -0500

    Add move_data_to_universal to git

---
+++ temoatools/move_data_to_universal_db.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Moves XLS into DB format using DB_Schema.db
+
+Written by Jeff Bennett, 11/16/2018
+JAB6ft@virginia.edu
+"""
+#=============================================================================
+# Imports
+#=============================================================================
+import os
+import shutil
+import sqlite3
+import pandas as pd
+import numpy as np
+import temoatools as tt
+
+
+def move_data_to_db(XLSX,path='.'):
+    
+    #=============================================================================
+    # Begin Function
+    #=============================================================================
+    workDir = os.getcwd()
+    os.chdir(path)
+
+    # Empty db with set schema (expected to be within the same folder)
+
+    emptydB  = tt.resource_path + "\\db_schema_universal.db"
+    
+    # Create output filename using inputfilename
+    outputdB = tt.remove_ext(XLSX) + ".db"
+    
+    # Keep track of sheet_names and corresponding number of columns to read-in
+    sheets = [("representativeDays",3),("timesOfDay",3),("Connections",11),("ConnectionsExisting",4),
+                   ("Demand",4),("DiscountRate",2),("Fuels",12),("FuelsExisting",4),("PowerPlants",8),
+                   ("PowerPlantsPerformance",9),("PowerPlantsCosts",7),("PowerPlantsConstraints",7),
+                   ("PowerPlantsExisting",4),("ReserveMargin",2),("capacityFactorTOD",5),("ref",6)]
+    
+    #----------
+    # sqlite file prep
+    #----------
+    # Delete old *.sqlite file (if it already exists) and copy/rename copy of temoa_schema.sqlite
+    if os.path.isfile(outputdB):
+        os.remove(outputdB)
+    shutil.copyfile(emptydB, outputdB)
+    
+    # Set-up sqlite connection
+    conn = sqlite3.connect(outputdB)
+    c = conn.cursor()
+    
+    #----------
+    # sqlite file prep
+    #----------
+    for sheet in sheets:
+        
+        # Extract sheet_name and number of columns for each sheet:
+        sheet_name = sheet[0]
+        sheet_col  = sheet[1]
+        
+        # Read XLS sheet
+        df = pd.read_excel(XLSX,sheetname = sheet_name)
+        df = df.drop([0]) # Remove first row (units)
+        
+        # Create SQL command based on number of entries
+        command = 'INSERT INTO ' + sheet_name + ' VALUES (?'
+        for i in range(sheet_col-1):
+            command = command + ',?'
+        command = command + ')'
+            
+        # Execute SQL command
+        c.executemany(command,np.array(df))
+    
+    #----------
+    # Save(commit) the changes and close sqlite file
+    #----------
+    conn.commit()
+    conn.close()
+
+    os.chdir(workDir)
+    return outputdB
+    #=============================================================================
+    # End Function
+    #=============================================================================
