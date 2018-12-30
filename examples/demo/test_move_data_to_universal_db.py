163049cd77ab0176a9ecdbdccd44e8eca0528c22
EnergyModels <jab6ft@virginia.edu>
2018-12-30 18:28:08 -0500

    Add test cases to git

---
+++ examples/demo/test_move_data_to_universal_db.py
@@ -0,0 +1,9 @@
+import temoatools as tt
+
+# file is expected to be in a sub-directory named 'data'
+filename = "data.xlsx"
+
+# build db from filename
+db = tt.move_data_to_db(filename,path='data')
+
+print "Successfully moved data to: " + db
