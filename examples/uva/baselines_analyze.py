aa92079105848a201d3bb98db70063b26618a866
Jeff Bennett <jab6ft@virginia.edu>
2019-01-16 14:09:46 -0500

    create uva model to evaluate energy storage options

---
+++ examples/uva/baselines_analyze.py
@@ -0,0 +1,65 @@
+import os
+import temoatools as tt
+#===============
+# Inputs
+#===============
+analyzeSingle = False
+onlySimple = False
+folder = dbFolder = os.getcwd() + '\\Databases'
+dbs = ["BATT.sqlite","CAES.sqlite","FLYWHL.sqlite"]
+
+#===============
+# Single (only first db)
+#===============
+if analyzeSingle == True:    
+    # Inputs
+    db = dbs[0]
+    createPlots =    'Y'         # Create default plots
+    saveData =       'Y'         # Do not save data as a csv or xls
+    sectorName =     'electric'  # Name of sector to be analyzed
+    
+    # Costs
+    yearlyCosts, LCOE = tt.getCosts(folder, db, saveData=saveData, createPlots=createPlots)
+    
+    # Emissions
+    yearlyEmissions, avgEmissions = tt.getEmissions(folder, db, saveData=saveData, createPlots=createPlots)
+
+    # Analyze capacity and activity by fuel types
+    switch = 'fuel'
+    capacityByFuel = tt.getCapacity(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+    ActivityByYearFuel = tt.getActivity(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+    ActivityByTODFuel = tt.getActivityTOD(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+
+    # Analyze capacity and activity by technology types
+    switch = 'tech'
+    capacityByTech = tt.getCapacity(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+    ActivityByYearFuel = tt.getActivity(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+    ActivityByTODTech = tt.getActivityTOD(folder, db, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+
+#===============
+# Multiple
+#===============
+else:
+    # Inputs:
+    createPlots =   'Y'        # Create default plots
+    saveData =      'Y'        # Do not save data as a csv or xls
+    sectorName=     'electric' # Name of sector to be analyzed
+
+    # Costs
+    yearlyCosts, LCOE = tt.getCosts(folder, dbs, saveData=saveData, createPlots=createPlots)
+
+    # Emissions
+    yearlyEmissions, avgEmissions = tt.getEmissions(folder, dbs, saveData=saveData,createPlots=createPlots)
+
+    if onlySimple == False:
+        # Analyze capacity and activity by fuel types
+        switch = 'fuel'
+        capacityByFuel = tt.getCapacity(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+        ActivityByYearFuel = tt.getActivity(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+        ActivityByTODFuel = tt.getActivityTOD(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+
+        # Analyze capacity and activity by technology types
+        switch = 'tech'
+        capacityByTech = tt.getCapacity(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+        ActivityByYearFuel = tt.getActivity(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
+        ActivityByTODTech = tt.getActivityTOD(folder, dbs, switch=switch,sectorName=sectorName,saveData=saveData,createPlots=createPlots)
