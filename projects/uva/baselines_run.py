376664afb1e5ecc2130178a71a0f6279855b9523
Jeff Bennett <jab6ft@virginia.edu>
2020-10-06 10:06:02 -0400

    reverted baselines_run.py to be the same as original temoatools repo
    adding energy storage duration part 1
    updated data.xlsx and db_schema_temoa_mod.sqlite to incorporate energy storage duration parameter

--- projects/uva/baselines_run.py
+++ projects/uva/baselines_run.py
@@ -1,6 +1,29 @@
+# ======================================================================================================================
+# baselines_run.py
+# Jeff Bennett, jab6ft@virginia.edu
+#
+# This script provides an example of using Temoatools to build and run Temoa models from two .xlsx files. The first
+# provides all possible system and technology data (named data.xlsx in the example). The second specifies scenarios
+# that make use of specified combinations of technology data (named Scenarios.xlsx in the example).
+#
+# Required inputs (lines 49-55)
+#   temoa_path - path to Temoa directory that contains temoa_model/
+#   project_path - path to directory that contains this file (expects a subdirectory within named data)
+#   modelInputs_XLSX_list - list that contains the *.xlsx file with model data (within data subdirectory)
+#   scenarioInputs - identifies which technologies are used for each scenario (within data subdirectory)
+#   scenarioNames_list - names of each scenario to be run from the scenarioInputs file (named within ScenarioInputs file)
+#   ncpus - number of cores to use, -1 for all, -2 for all but one, replace with int(os.getenv('NUM_PROCS')) for cluster
+#   solver - leave as '' to use system default, other options include 'cplex', 'gurobi'
+#
+# Outputs (paths are all relative to project_path)
+#   data/data.db - universal database that contains input data in a .sqlite database
+#   configs/config_*.txt - a separate configuration file for each Temoa run
+#   databases/*.dat - a separate .sqlite database for each Temoa run
+#   databases/*.sqlite - a separate .sqlite database for each Temoa run
+# ======================================================================================================================
 import temoatools as tt
-import multiprocessing
 from joblib import Parallel, delayed, parallel_backend
+from pathlib import Path
 import os
 
 
@@ -23,41 +46,48 @@ if __name__ == '__main__':
     # =======================================================
     # Model Inputs
     # =======================================================
-    modelInputs_XLSX = 'data.xlsx'
-    scenarioInputs = 'scenarios.xlsx'
-    scenarioNames = ['BATT']
-    paths = 'paths.csv'
-
     temoa_path = os.path.abspath('../../temoa-energysystem')
     project_path = os.getcwd()
-    solver = ''  # 'gurobi'
+    modelInputs_XLSX_list = ['data.xlsx']
+    scenarioInputs = 'scenarios.xlsx'
+    scenarioNames_list = [['A', 'B', 'C', 'D', 'E', 'F']]
+    ncpus = 6   # default, unless otherwise specified in sbatch script
+    solver = ''  # leave blank to let temoa decide which solver to use of those installed
 
     # =======================================================
-    # Move modelInputs_XLSX to database
+    # begin script
     # =======================================================
-    modelInputs = tt.move_data_to_db(modelInputs_XLSX, path=project_path)
+    try:
+        ncpus = int(os.getenv('NUM_PROCS'))  # try to use variable defined in sbatch script
+    except:
+        ncpus = ncpus  # otherwise default to this number of cores
 
-    # Build Model
-    tt.build(modelInputs, scenarioInputs, 'Batt', 'Batt', path=project_path)
+    for modelInputs_XLSX, scenarioNames in zip(modelInputs_XLSX_list, scenarioNames_list):
 
-    # Run Model
-    tt.run('Batt', saveEXCEL=False, temoa_path=temoa_path, debug=True, solver=solver)
+        # =======================================================
+        # Move modelInputs_XLSX to database
+        # =======================================================
+        modelInputs = tt.move_data_to_db(modelInputs_XLSX, path=project_path)
 
-    # ====================================
-    # Perform Simulations
-    option = 2  # 1 - Run single, 2 - Run all
-    # ====================================
-    num_cores = multiprocessing.cpu_count() - 1  # Save one core for other processes
+        # =======================================================
+        # Create directories - best completed before using multiprocessing
+        # =======================================================
+        tt.create_dir(project_path=project_path, optional_dir='results')
 
-    if option == 1:
-        # Perform single simulation
-        evaluateModel(modelInputs, scenarioInputs, scenarioNames[0], temoa_path)
+        # ====================================
+        # Perform Simulations
+        option = 2  # 1 - Run single, 2 - Run all
+        # ====================================
 
-    elif option == 2:
-        # Perform simulations in parallel
-        with parallel_backend('multiprocessing', n_jobs=num_cores):
-            Parallel(n_jobs=num_cores, verbose=5)(
-                delayed(evaluateModel)(modelInputs, scenarioInputs, scenarioName, temoa_path, project_path, solver)
-                for
-                scenarioName in
-                scenarioNames)
+        if option == 1:
+            # Perform single simulation
+            evaluateModel(modelInputs, scenarioInputs, scenarioNames[0], temoa_path)
+
+        elif option == 2:
+            # Perform simulations in parallel
+            with parallel_backend('multiprocessing', n_jobs=ncpus):
+                Parallel(n_jobs=ncpus, verbose=5)(
+                    delayed(evaluateModel)(modelInputs, scenarioInputs, scenarioName, temoa_path, project_path, solver)
+                    for
+                    scenarioName in
+                    scenarioNames)
